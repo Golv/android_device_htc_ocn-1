fe1746343cdc2453a7422682107180af4cdacdfe
golv <lok02@list.ru>
2019-11-18 15:26:18 +0300

    ocn: update device tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,13 +10,13 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := kryo
+TARGET_CPU_VARIANT := cortex-a73
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := kryo
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
 TARGET_NO_BOOTLOADER := true
 
@@ -42,6 +42,7 @@ BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100 --board recovery:0
 
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_PREBUILT_KERNEL := device/htc/ocn/prebuilt/Image.lz4-dtb
 
 #TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,5 @@ export LC_ALL=C
 export ALLOW_MISSING_DEPENDENCIES=true
 export TARGET_COMPILE_WITH_MSM_KERNEL=false
 
+add_lunch_combo omni_ocn-eng
+add_lunch_combo omni_ocn-userdebug
